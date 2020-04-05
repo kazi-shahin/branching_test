3e1832258cdf0e162a258e4d22f4e5e59334441d
Muhin <muhin@blubirdinteractive.com>
2020-04-05 17:12:51 +0600

    Update project description

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
 <?php
 
 echo "Hello git test";
+
+echo "<br>";
+
+echo "This is a project for testing git branches. We want to create a demo branch. Then we will change in demo branch. And then we will marge the changes to master branch.";
